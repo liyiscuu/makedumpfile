a3ba4dfed0f70480b64592a64faca38b2fa628a3
Petr Tesarik <ptesarik@suse.com>
2018-07-20 07:47:54 +0200

    Cleanup: Remove unused functions
    These functions are unused and can be removed:
    
    - page_head_to_phys_start
    - page_head_to_phys_end
    - offset_to_pt_load_start
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- elf_info.h
+++ elf_info.h
@@ -32,9 +32,6 @@ int get_elf32_phdr(int fd, char *filename, int index, Elf32_Phdr *phdr);
 
 off_t paddr_to_offset(unsigned long long paddr);
 off_t paddr_to_offset2(unsigned long long paddr, off_t hint);
-unsigned long long page_head_to_phys_start(unsigned long long head_paddr);
-unsigned long long page_head_to_phys_end(unsigned long long head_paddr);
-off_t offset_to_pt_load_start(off_t offset);
 off_t offset_to_pt_load_end(off_t offset);
 unsigned long long vaddr_to_paddr_general(unsigned long long vaddr);
 off_t vaddr_to_offset_slow(int fd, char *filename, unsigned long long vaddr);
